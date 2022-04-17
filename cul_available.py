48c3618c5252204d897467828ea495a97ffb922a
yule <tttt@xiyoulinux.org>
2022-04-17 23:44:26 +0800

    avaliable

---
+++ cul_available.py
@@ -0,0 +1,45 @@
+#统计进程的可用内存
+
+from __future__ import print_function
+from time import sleep, strftime
+from bcc import BPF
+
+bpf_text = """
+
+#include <uapi/linux/ptrace.h>
+
+#define PAGE_SHIFT 12
+#define K(x) (x) << (PAGE_SHIFT - 10)
+
+BPF_HASH(table_available, u32, u32);
+
+int cul_available(struct pt_regs *ctx) {
+    
+    u32 return_value;
+
+    return_value = PT_REGS_RC(ctx);
+    return_value = K(return_value);
+
+
+    u32 temp = 1;
+    table_available.update(&temp, &return_value);
+
+    
+    return 0;
+}
+
+
+
+
+"""
+
+b = BPF(text = bpf_text)
+b.attach_kretprobe(event = "si_mem_available", fn_name = "cul_available")
+table_available = b.get_table("table_available")
+
+while(1):
+    sleep(1)
+    for k, v in table_available.items():
+        print("MEMavailable(可用): %lu"%(v.value))
+
+
